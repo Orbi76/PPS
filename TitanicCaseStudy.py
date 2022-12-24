59d0e82ca8013d39f742bf30959d6329abcbf869
Orbi76 <oorbangabor@gmail.com>
2022-12-24 00:31:50 +0000

    Titanic task 1

--- TitanicCaseStudy.py
+++ TitanicCaseStudy.py
@@ -4,4 +4,23 @@ from pathlib import Path
 data_folder = Path("/Users/gabor/Documents/Solent/5. év Masters/Computer Fundamentals/Python/")
 
 file_to_open = data_folder / "titanic.csv"
+
+records = []
+print("Loading data...", end="")
+
 f = open(file_to_open)
+reader = csv.reader(f)
+header = []
+header = next(reader)
+
+
+for row in reader:
+  records.append(row)
+
+f.close()
+print(" Done!")
+print (header)
+a = len(records)
+#print(a)
+#a = str a
+print(f"Successfully loaded {a} records.")
