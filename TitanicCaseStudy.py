f019f08cb6edba5e82cb4b5de7169516cb8d2bde
Orbi76 <oorbangabor@gmail.com>
2022-12-24 00:56:13 +0000

    Titanic task 2 done

--- TitanicCaseStudy.py
+++ TitanicCaseStudy.py
@@ -19,3 +19,11 @@ for row in reader:
 f.close()
 print(" Done!")
 print(f"Successfully loaded {len(records)} records.")
+
+print("Please select one of the following options:")
+print("[1] Display the names of all passengers")
+print("[2] Display the number of passengers that survived")
+print("[3] Display the number of passengers per gender")
+print("[4] Display the number of passengers per age group")
+response = int(input())
+print(f"You have selected option: {response} ")
