4e47b88303d3d7c3275e9b801ff9ce7484dc54e5
Orbi76 <oorbangabor@gmail.com>
2022-12-29 14:24:43 +0000

    corrected to solution

--- tui.py
+++ tui.py
@@ -1,26 +1,33 @@
+
+LINE_WIDTH = 85
 def started(msg=""):
-    print("-------------------------------------------------------------------------------------")
-    print(f"Operation started: {msg}...\n")
+    output = f"Operation started: {msg}..."
+    dashes = "-" * LINE_WIDTH
+   # print("-------------------------------------------------------------------------------------")
+    print(f"{dashes}\n {output}\n")
 
 
 def completed():
-    print("")
-    print("Operation completed.")
-    print("-------------------------------------------------------------------------------------")
+    dashes = "-" * LINE_WIDTH
+    print(f"\nOperation completed.\n{dashes}\n")
+   # print("-------------------------------------------------------------------------------------")
 
 def error(msg):
-    print(f"Error! {msg}!")
+    print(f"Error! {msg}\n")
 
 
 def menu():
-    print(f"Please select on of the following options:")
-    print(f"[years]    List unique years")
-    print(f"[tally]    Tally up medals")
-    print(f"[ctally]    Tally up medals for each team")
-    print(f"[exit]    Exit the program")
-    response = input()
-    print(f"Your selection: {response} ")
+    print(f"""Please select on of the following options:
+    {"[years]":<10} List unique years
+    {"[tally]":<10} Tally up medals
+    {"[team]":<10} Tally up medals for each team
+    {"[exit]":<10} Exit the program
+    """)
+    selection = input("Your selection: ")
+    return selection.strip().lower()
 
+
+"""
 tally = {
         "Gold": "13372",
         "Silver": "222",
@@ -32,7 +39,7 @@ team_tally = {
 "Country2": {"Name": "Germany", "Gold": 15, "Silver": 16, "Bronze": 17},
 "Country3": {"Name": "Hungary", "Gold": 20, "Silver": 50, "Bronze": 200}
 }
-
+"""
 
 def display_medal_tally(tally):
     numberOfGoldMedals = tally.get("Gold")
@@ -44,18 +51,35 @@ def display_medal_tally(tally):
     print(f"| Bronze     |  {numberOfBronzeMedals}  |")
 
 def display_team_medal_tally(team_tally):
+    for team, tally in team_tally.items():
+        print(team)
+        print(f"\tGold:{tally['Gold']}, Silver:{tally['Silver']}, Bronze:{tally['Bronze']}")
+"""        
     numberOfGoldMedals = team_tally.get("Gold")
     numberOfSilverMedals = team_tally.get("Silver")
     numberOfBronzeMedals = team_tally.get("Bronze")
+    
+ 
     for i in team_tally:
         print(team_tally[i].get("Name"))
         print(f"\t\tGold:"+str(team_tally[i].get("Gold"))+", Silver:"+str(team_tally[i].get("Silver"))+", Bronze:"+str(team_tally[i].get("Bronze")))
+"""
 
+#years = {"2022", "2021","2023", "2020"}
 
+def display_years(years):
+    sorted_years = sorted(years, reverse=True)
+    for row in sorted_years:
+        print(row)
 
+"""
 started("ezt")
 completed()
 error("Hiba")
 menu()
 display_medal_tally(tally)
 display_team_medal_tally(team_tally)
+display_years(years)
+
+"""
+#menu()
